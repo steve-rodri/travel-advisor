fe3c7b0601beb2b6502bd92a7da5d6466e04aa7b
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-03 21:33:09 -0500

    implement getPlacesData api

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const { data } = await axios({
+      method: "GET",
+      url: "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary",
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+      },
+    });
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
