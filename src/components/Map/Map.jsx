d1f7bfd5528caca7d71b07b367451f925ed7ca3c
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-02-06 12:36:31 -0500

    refactor Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import GoogleMapReact from "google-map-react";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
@@ -7,79 +7,86 @@ import { Rating } from "@material-ui/lab";
 import useStyles from "./styles";
 import mapStyles from "./mapStyles";
 
-const Map = ({
-  places,
-  coordinates,
-  setCoordinates,
-  setChildClicked,
-  setBounds,
-  weatherData,
-}) => {
+export const Map = props => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(max-width: 600px)");
+  const onChange = e => {
+    props.setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+    props.setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+  };
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-        defaultCenter={coordinates}
-        center={coordinates}
+        center={props.coordinates}
+        defaultCenter={props.coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
+        onChange={onChange}
+        onChildClick={child => props.setChildClicked(child)}
         options={{
           disableDefaultUI: true,
           zoomControl: true,
           styles: mapStyles,
         }}
-        onChange={e => {
-          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
-          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
-        }}
-        onChildClick={child => setChildClicked(child)}
       >
-        {places?.map((place, i) => (
-          <div
-            className={classes.markerContainer}
-            lat={Number(place.latitude)}
-            lng={Number(place.longitude)}
-            key={i}
-          >
-            {isMobile ? (
-              <LocationOnOutlinedIcon color="primary" fontSize="large" />
-            ) : (
-              <Paper elevation={3} className={classes.paper}>
-                <Typography
-                  className={classes.typography}
-                  variant="subtitle2"
-                  gutterBottom
-                >
-                  {place.name}
-                </Typography>
-                <img
-                  className={classes.pointer}
-                  alt={place.name}
-                  src={
-                    place.photo
-                      ? place.photo.images.large.url
-                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
-                  }
-                />
-                <Rating size="small" value={Number(place.rating)} readOnly />
-              </Paper>
-            )}
-          </div>
-        ))}
-        {weatherData?.list?.map((data, i) => (
-          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-            <img
-              height={100}
-              src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-              alt="weather"
-            />
-          </div>
-        ))}
+        <Places {...props} classes={classes} isMobile={isMobile} />
+        <WeatherData {...props} />
       </GoogleMapReact>
     </div>
   );
+};
+
+const Places = props => {
+  const { places, classes } = props;
+  return places?.map((place, i) => (
+    <div
+      className={classes.markerContainer}
+      lat={Number(place.latitude)}
+      lng={Number(place.longitude)}
+      key={i}
+    >
+      <Place {...props} {...place} />
+    </div>
+  ));
+};
+
+const Place = props => {
+  const { isMobile, rating, classes } = props;
+  if (isMobile)
+    return <LocationOnOutlinedIcon color="primary" fontSize="large" />;
+  return (
+    <Paper elevation={3} className={classes.paper}>
+      <PlaceName {...props} />
+      <PlacePhoto {...props} />
+      <Rating size="small" value={Number(rating)} readOnly />
+    </Paper>
+  );
+};
+
+const PlaceName = ({ name, classes }) => (
+  <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+    {name}
+  </Typography>
+);
+
+const PlacePhoto = ({ photo, name, classes }) => {
+  const src = photo
+    ? photo.images.large.url
+    : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg";
+  return <img className={classes.pointer} alt={name} src={src} />;
+};
+
+const WeatherData = ({ weatherData }) => {
+  return weatherData?.list?.map((data, i) => (
+    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+      <img
+        height={100}
+        src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+        alt="weather"
+      />
+    </div>
+  ));
 };
 
 export default Map;
